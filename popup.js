6ece900f9b9628e774bb170e97eb25784eb2644f
Robert Bieber <github@biebersprojects.com>
2018-04-07 01:12:02 -0400

    Another bugfix

--- popup.js
+++ popup.js
@@ -44,7 +44,7 @@ chrome.runtime.onMessage.addListener(
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, {fetch: 'code'}, function(response) {
-	  setCode(response.code);
+	  setCode(response && response.code);
   });
 });
 
